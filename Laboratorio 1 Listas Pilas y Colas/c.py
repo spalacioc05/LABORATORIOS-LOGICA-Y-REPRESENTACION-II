8d697cbc385491b5ad98e52fa92bdb70a076346d
Santiago <143017424+spalacioc05@users.noreply.github.com>
2024-09-06 15:20:10 -0500

    aa
    a

--- Laboratorio 1 Listas Pilas y Colas/c.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-def generar_letra_aleatoria():
-    return random.choice(string.ascii_letters)
-
-# Generar y mostrar una letra al azar
-letra_aleatoria = generar_letra_aleatoria()
-print("Letra aleatoria:", letra_aleatoria)
