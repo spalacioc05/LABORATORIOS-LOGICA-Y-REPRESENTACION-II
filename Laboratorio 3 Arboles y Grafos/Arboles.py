a6ea960b5ef1be1778199a3ff12c756ab27b9048
Santiago Palacio <spalacioc05@gmail.com>
2024-11-18 23:36:07 -0500

    Versión beta

--- Laboratorio 3 Arboles y Grafos/Arboles.py
+++ Laboratorio 3 Arboles y Grafos/Arboles.py
@@ -1,37 +1,38 @@
 import matplotlib.pyplot as plt
 
-#Clase Nodo sacada de las diapositivas de clase
 class Nodo:
     def __init__(self, valor):
         self.valor = valor
         self.izquierda = None
         self.derecha = None
 
-
 class ArbolBinario:
     def __init__(self):
         self.raiz = None
 
-    #Método para insertar sacado de las diapositivas de clase
     def insertar(self, valor):
-        nuevo_nodo = Nodo(valor)
+        nuevoNodo = Nodo(valor)
+        
         if self.raiz is None:
-            self.raiz = nuevo_nodo
+            self.raiz = nuevoNodo
+            
         else:
             actual = self.raiz
             while actual:
                 padre = actual
                 if valor < actual.valor:
                     actual = actual.izquierda
+                    
                 else:
                     actual = actual.derecha
+                    
             if valor < padre.valor:
-                padre.izquierda = nuevo_nodo
+                padre.izquierda = nuevoNodo
+                
             else:
-                padre.derecha = nuevo_nodo
+                padre.derecha = nuevoNodo
 
-    #Método recorrido_inorden_no_recursivo sacados de las diapositivas
-    def recorrido_inorden_no_recursivo(self):
+    def recorridoInordenNoRecursivo(self):
         pila = []
         actual = self.raiz
         valores = []
@@ -40,6 +41,7 @@ class ArbolBinario:
             if actual:
                 pila.append(actual)
                 actual = actual.izquierda
+                
             else:
                 actual = pila.pop()
                 valores.append(actual.valor)
@@ -47,8 +49,7 @@ class ArbolBinario:
 
         return valores
 
-    #Método recorrido_preorden_no_recursivo sacado de las diapositivas de clase
-    def recorrido_preorden_no_recursivo(self):
+    def recorridoPreordenNoRecursivo(self):
         if self.raiz is None:
             return
 
@@ -59,56 +60,72 @@ class ArbolBinario:
             nodo = pila.pop()
             print(nodo.valor, end=' ')
 
-            # Agregar el hijo derecho antes del hijo izquierdo en la pila
             if nodo.derecha:
                 pila.append(nodo.derecha)
+                
             if nodo.izquierda:
                 pila.append(nodo.izquierda)
 
-    #Método para balancear el árbol actual
     def balancear(self):
-        valores_ordenados = self.recorrido_inorden_no_recursivo() #primero ordenamos
-        self.raiz = self.crear_arbol_balanceado(valores_ordenados)
+        if not self.estaBalanceado(self.raiz):
+            valoresOrdenados = self.recorridoInordenNoRecursivo()
+            self.raiz = self.crearArbolBalanceado(valoresOrdenados)
 
-
-    #Método para construir el árbol balanceado a partir de la lista con valores ordenados."""
-    def crear_arbol_balanceado(self, valores_ordenados):
-        if not valores_ordenados:
+    def crearArbolBalanceado(self, valoresOrdenados):
+        if not valoresOrdenados:
             return None
-        mid = len(valores_ordenados) // 2
-        nodo = Nodo(valores_ordenados[mid])
-        nodo.izquierda = self.crear_arbol_balanceado(valores_ordenados[:mid])
-        nodo.derecha = self.crear_arbol_balanceado(valores_ordenados[mid + 1:])
+        
+        mid = len(valoresOrdenados) // 2
+        nodo = Nodo(valoresOrdenados[mid])
+        nodo.izquierda = self.crearArbolBalanceado(valoresOrdenados[:mid])
+        nodo.derecha = self.crearArbolBalanceado(valoresOrdenados[mid + 1:])
+        
         return nodo
 
+    def estaBalanceado(self, nodo):
+        if nodo is None:
+            return True
 
+        alturaIzquierda = self.altura(nodo.izquierda)
+        alturaDerecha = self.altura(nodo.derecha)
 
-    # Método para crear una representación gráfica del árbol
-    def graficar_arbol(self):
+        if abs(alturaIzquierda - alturaDerecha) <= 1 and self.estaBalanceado(nodo.izquierda) and self.estaBalanceado(nodo.derecha):
+            return True
+
+        return False
+
+    def altura(self, nodo):
+        if nodo is None:
+            return 0
+        
+        return max(self.altura(nodo.izquierda), self.altura(nodo.derecha)) + 1
+
+    def graficarArbol(self):
         if not self.raiz:
             print("El árbol está vacío.")
             return
 
-        def recorrer_nodos(nodo, x, y, dx, posiciones, conexiones):
+        def recorrerNodos(nodo, x, y, dx, posiciones, conexiones):
             if nodo:
                 posiciones[nodo.valor] = (x, y)
+                
                 if nodo.izquierda:
                     conexiones.append((nodo.valor, nodo.izquierda.valor))
-                    recorrer_nodos(nodo.izquierda, x - dx, y - 1, dx / 2, posiciones, conexiones)
+                    recorrerNodos(nodo.izquierda, x - dx, y - 1, dx / 2, posiciones, conexiones)
+                    
                 if nodo.derecha:
                     conexiones.append((nodo.valor, nodo.derecha.valor))
-                    recorrer_nodos(nodo.derecha, x + dx, y - 1, dx / 2, posiciones, conexiones)
+                    recorrerNodos(nodo.derecha, x + dx, y - 1, dx / 2, posiciones, conexiones)
 
         posiciones = {}
         conexiones = []
-        recorrer_nodos(self.raiz, 0, 0, 1, posiciones, conexiones)
+        
+        recorrerNodos(self.raiz, 0, 0, 1, posiciones, conexiones)
 
-        # Graficar los nodos
         for valor, (x, y) in posiciones.items():
             plt.scatter(x, y, s=500, zorder=2)
             plt.text(x, y, str(valor), ha='center', va='center', fontsize=12, zorder=3)
 
-        # Graficar las conexiones
         for de, a in conexiones:
             x1, y1 = posiciones[de]
             x2, y2 = posiciones[a]
@@ -117,12 +134,10 @@ class ArbolBinario:
         plt.axis('off')
         plt.show()
 
-#Método con el menu y acciones a ejecutar
-def gestionar_arbol():
-    arbol = ArbolBinario() # instanciamos nuestro arbol vacío con todos lo métodos necesario
+def gestionarArbol():
+    arbol = ArbolBinario()
 
     while True:
-
         print("Opciones:")
         print("1. Insertar números en el árbol")
         print("2. Balancear el árbol")
@@ -132,7 +147,7 @@ def gestionar_arbol():
         opcion = input("Seleccione una opción: ")
 
         if opcion == "1":
-            numeros = input("Ingrese una serie de números separados por espacios: ")
+            numeros = input("Ingrese los números separados por espacios: ")
             for num in map(int, numeros.split()):
                 arbol.insertar(num)
             print("Números insertados en el árbol (sin balancear): ", numeros.split())
@@ -140,12 +155,12 @@ def gestionar_arbol():
         elif opcion == "2":
             arbol.balancear()
             print("Árbol balanceado en recorrido preorden:")
-            arbol.recorrido_preorden_no_recursivo()
+            arbol.recorridoPreordenNoRecursivo()
             print()
 
         elif opcion == "3":
             print("\nRepresentación gráfica del árbol:")
-            arbol.graficar_arbol()
+            arbol.graficarArbol()
 
         elif opcion == "4":
             print("Programa finalizado.")
@@ -155,4 +170,4 @@ def gestionar_arbol():
             print("Opción no válida. Intente de nuevo.")
 
 
-gestionar_arbol()
+gestionarArbol()
--- Laboratorio 3 Arboles y Grafos/Grafos.py
+++ Laboratorio 3 Arboles y Grafos/Grafos.py
@@ -2,73 +2,72 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import random
 
-# Crear grafo con un ciclo for para agregar nodos
-def crear_grafo_con_nodos():
-    grafo = nx.Graph()  # Crea un grafo vacío
+def crearGrafo():
+    grafo = nx.Graph()
+    
     for id in range(100):
-        grafo.add_node(id)  # Agrega cada id como nodo
+        grafo.add_node(id)
+        
     return grafo
 
-grafo = crear_grafo_con_nodos()
-
-def agregar_conexiones_aleatorias(grafo):
+def agregarConexionesAleatorias(grafo):
     nodos = list(grafo.nodes)
-
+    
     for nodo in nodos:
-        conexiones = random.randint(5, 10)  # Numero aleatorio de conexiones entre 5 y 10
-        posibles_amigos = set(nodos) - {nodo}  # Excluir al nodo actual para que no haya relación consigo él mismo)
-        amigos = random.sample(sorted(posibles_amigos), min(conexiones, len(posibles_amigos)))  # Elegir nodos aleatorios
+        conexiones = random.randint(5, 10)
+        posiblesAmigos = set(nodos) - {nodo}
+        amigos = random.sample(sorted(posiblesAmigos), min(conexiones, len(posiblesAmigos)))
+        
         for amigo in amigos:
-            grafo.add_edge(nodo, amigo)  # Crear una relacion entre el nodo actual y el amigo seleccionado
+            grafo.add_edge(nodo, amigo)
 
+def encontrarAmigosComunes(grafo, amigoA, amigoB):
+    amigosPersonaA = set(grafo.neighbors(amigoA))
+    amigosPersonaB = set(grafo.neighbors(amigoB))
+    amigosComunes = amigosPersonaA.intersection(amigosPersonaB)
+    
+    return amigosComunes
 
-agregar_conexiones_aleatorias(grafo)
-
-amigo_a = int(input("Ingrese id de la primera persona (número entre0 y 99): "))
-amigo_b = int(input("Ingrese id de la segunda persona (número entre0 y 99): "))
-
-
-def encontrar_amigos_comunes(grafo, amigo_a, amigo_b):
-    amigos_persona_a = set(grafo.neighbors(amigo_a))
-    amigos_persona_b = set(grafo.neighbors(amigo_b))
-
-
-    amigos_comunes = amigos_persona_a.intersection(amigos_persona_b)
-    return amigos_comunes
-
-amigos_comunes = encontrar_amigos_comunes(grafo, amigo_a, amigo_b)
-print(f"Amigos en común entre {amigo_a} y {amigo_b}: {amigos_comunes}")
-
-def dibujar_grafo_con_destacados(grafo, amigo_a, amigo_b, amigos_comunes):
+def dibujarGrafoConAmigos(grafo, amigoA, amigoB, amigosComunes):
+    plt.figure(figsize=(15, 15))
     pos = nx.spring_layout(grafo)
 
-    # Colores para los nodos
-    colores_nodos = []
+    coloresNodos = []
+    
     for nodo in grafo.nodes:
-        if nodo == amigo_a or nodo == amigo_b:
-            colores_nodos.append("green")
-        elif nodo in amigos_comunes:
-            colores_nodos.append("orange")
+        if nodo == amigoA or nodo == amigoB:
+            coloresNodos.append("green")
+            
+        elif nodo in amigosComunes:
+            coloresNodos.append("orange")
+            
         else:
-            colores_nodos.append("lightblue")
+            coloresNodos.append("lightblue")
 
-    # Colores para las aristas
-    colores_aristas = []
+    coloresAristas = []
+    
     for u, v in grafo.edges:
-        if (u in amigos_comunes and v in amigos_comunes) or \
-           (u in amigos_comunes and (v == amigo_a or v == amigo_b)) or \
-           (v in amigos_comunes and (u == amigo_a or u == amigo_b)):
-            colores_aristas.append("red")  # Conexion de amigos
+        if (u in amigosComunes and v in amigosComunes) or (u in amigosComunes and (v == amigoA or v == amigoB)) or (v in amigosComunes and (u == amigoA or u == amigoB)):
+            coloresAristas.append("red")
+            
         else:
-            colores_aristas.append("gray")  # Otros..
+            coloresAristas.append("gray")
 
+    nx.draw_networkx_nodes(grafo, pos, node_color=coloresNodos, node_size=900)
+    nx.draw_networkx_edges(grafo, pos, edge_color=coloresAristas)
+    nx.draw_networkx_labels(grafo, pos, font_size=10)
 
-    nx.draw_networkx_nodes(grafo, pos, node_color=colores_nodos, node_size=700) #Nodos
-    nx.draw_networkx_edges(grafo, pos, edge_color=colores_aristas) #Aristas
-    nx.draw_networkx_labels(grafo, pos, font_size=10) #Etiquetas
-
-    plt.title(f"Amigos en común entre {amigo_a} y {amigo_b}")
+    plt.title(f"Amigos en común entre {amigoA} y {amigoB}", fontsize=20)
     plt.axis("off")
     plt.show()
 
-dibujar_grafo_con_destacados(grafo, amigo_a, amigo_b, amigos_comunes)
+
+grafo = crearGrafo()
+agregarConexionesAleatorias(grafo)
+
+amigoA = int(input("Ingrese id de la primera persona (número entre 0 y 99): "))
+amigoB = int(input("Ingrese id de la segunda persona (número entre 0 y 99): "))
+amigosComunes = encontrarAmigosComunes(grafo, amigoA, amigoB)
+
+print(f"Amigos en común entre {amigoA} y {amigoB}: {amigosComunes}")
+dibujarGrafoConAmigos(grafo, amigoA, amigoB, amigosComunes)
